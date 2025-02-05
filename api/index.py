16d2b78b5c86f06c2b41421c57b1f95e41d8b6aa
Deborah Dahl <dahl@conversational-technologies.com>
2025-02-05 17:14:32 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,1 +1,31 @@
+# The latest working min Server code 20231105
+# =================================================
+# Note!!!! you will need to install flask_cors
+#    open a bash console and do this
+#    pip3.10 install --user flask_cors
 
+from flask import Flask
+from flask import request
+from flask_cors import CORS
+import json
+import assistant
+
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
+port = 8767
+
+@app.route('/', methods=['POST'])
+def home():
+    inputOVON = json.loads( request.data )
+
+    host = request.host.split(":")[0]
+    sender_from = f"http://{host}"
+    ovon_response = assistant.generate_response(inputOVON, sender_from)
+
+    return ovon_response
+
+def handler(environ, start_response):
+    return app(environ, start_response)
+
+if __name__ == '__main__':
+    app.run(host="localhost",port=port, debug=True, use_reloader=False)
